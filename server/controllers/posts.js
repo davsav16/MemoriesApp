2d1de5eb1cc0b2715aa708d8dde0dbf50287587b
DavidM <david.mcdougal00@gmail.com>
2021-10-23 22:24:40 -0600

    added the ability to delete a post

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -49,10 +49,12 @@ export const updatePost = async (req, res) => {
 
 export const deletePost = async (req, res) => {
   const { id } = req.params;
-  if (!mongoose.Types.ObjectId.isValid(_id))
+  if (!mongoose.Types.ObjectId.isValid(id))
     return res.status(404).send("No post with that id");
 
   await PostMessage.findByIdAndRemove(id);
+
+  console.log("DELETE");
 
   res.json({ message: "Post deleted succesfully" });
 };
